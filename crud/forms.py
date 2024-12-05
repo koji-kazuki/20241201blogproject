abbf891773bd913d23dbe452a1bd04000aee0511
koji-kazuki <kazu.k0612@outlook.jp>
2024-12-06 02:57:05 +0900

    test

--- crud/forms.py
+++ crud/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired
 
+
 class AdminForm(FlaskForm):
 
     username = StringField(
